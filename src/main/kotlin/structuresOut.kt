a0483a1c4f078de31c7cb3f153fe3908f1c9eb80
first_project_Shefer <lora0207@list.ru>
2021-10-15 16:30:15 +0300

    structuresOut.kt was refactored

--- src/main/kotlin/structuresOut.kt
+++ src/main/kotlin/structuresOut.kt
@@ -3,73 +3,65 @@ import org.jetbrains.skija.Font
 import org.jetbrains.skija.Paint
 import org.jetbrains.skija.Rect
 
-
-
+/*
+ * out point
+ */
 fun pointOut(instruction : Array<Any>, canvas : Canvas, paint : Paint, font : Font){
     canvas.drawPoint(instruction[1].toString().toFloat(), instruction[2].toString().toFloat(), paint)
 }
 
+/*
+ * out arc
+ */
 fun ArcOut(instruction : Array<Any>, canvas : Canvas, paint : Paint, font : Font){
     canvas.drawArc(instruction[1].toString().toFloat(), instruction[2].toString().toFloat(), instruction[3].toString().toFloat(), instruction[4].toString().toFloat(),
         instruction[5].toString().toFloat(), instruction[6].toString().toFloat(), true, Paint().apply{color = instruction[7].toString().toInt()})
 }
+
 /*
- * Выводим линию по инструкции
+ * out line
  */
 fun lineOut(instruction : Array<Any>, canvas : Canvas, paint : Paint, font : Font) {
-    var x1 = instruction[1].toString().toFloatOrNull()
-    var y1 = instruction[2].toString().toFloatOrNull()
-    var x2 = instruction[3].toString().toFloatOrNull()
-    var y2 = instruction[4].toString().toFloatOrNull()
-    var color1 = instruction[5].toString().toIntOrNull()
-    if (x1 == null || y1 == null || x2 == null || y2 == null || color1 == null) {
-        println("Error")
-        return
-    }
+    var x1 = instruction[1].toString().toFloat()
+    var y1 = instruction[2].toString().toFloat()
+    var x2 = instruction[3].toString().toFloat()
+    var y2 = instruction[4].toString().toFloat()
+    var color1 = instruction[5].toString().toInt()
     canvas.drawLine(x1, y1, x2, y2, Paint().apply { color = color1 })
 }
 
 /*
- * Выводим строку по инструкции
+ * out string
  */
 fun stringOut(instruction : Array<Any>, canvas : Canvas, paint : Paint, font : Font){
     var str = instruction[1].toString()
-    var x1 = instruction[2].toString().toFloatOrNull()
-    var y1 = instruction[3].toString().toFloatOrNull()
-    if(x1 == null || y1 == null){
-        println("Error")
-        return
-    }
+    var x1 = instruction[2].toString().toFloat()
+    var y1 = instruction[3].toString().toFloat()
     canvas.drawString(str, x1, y1, font, paint)
 }
 
 /*
- * Вывод прямоугольника по инструкции
+ * out rectangle
  */
 fun rectOut(instruction : Array<Any>, canvas : Canvas, paint : Paint, font : Font){
-    var x1 = instruction[1].toString().toFloatOrNull()
-    var y1 = instruction[2].toString().toFloatOrNull()
-    var x2 = instruction[3].toString().toFloatOrNull()
-    var y2 = instruction[4].toString().toFloatOrNull()
-    var color1 = instruction[5].toString().toLongOrNull()
-    if(x1 == null || y1 == null || x2 == null || y2 == null || color1 == null){
-        println("Error")
-        return
-    }
+    var x1 = instruction[1].toString().toFloat()
+    var y1 = instruction[2].toString().toFloat()
+    var x2 = instruction[3].toString().toFloat()
+    var y2 = instruction[4].toString().toFloat()
+    var color1 = instruction[5].toString().toLong()
     canvas.drawRect(Rect.makeXYWH(x1, y1, x2, y2), Paint().apply{color = color1.toInt()})
 }
 
+/*
+ * out rectangle colored with gradient
+ */
 fun rectShaderOut(instruction : Array<Any>, canvas : Canvas, paint : Paint, font : Font){
-    var x1 = instruction[1].toString().toFloatOrNull()
-    var y1 = instruction[2].toString().toFloatOrNull()
-    var x2 = instruction[3].toString().toFloatOrNull()
-    var y2 = instruction[4].toString().toFloatOrNull()
-    var color1 = instruction[5].toString().toIntOrNull()
-    var color2 = instruction[6].toString().toIntOrNull()
-    if(x1 == null || y1 == null || x2 == null || y2 == null || color1 == null || color2 == null){
-        println("Error")
-        return
-    }
+    var x1 = instruction[1].toString().toFloat()
+    var y1 = instruction[2].toString().toFloat()
+    var x2 = instruction[3].toString().toFloat()
+    var y2 = instruction[4].toString().toFloat()
+    var color1 = instruction[5].toString().toInt()
+    var color2 = instruction[6].toString().toInt()
     var paint2 = paint
     paint2.shader = createShader(x1, x2,
         Paint().apply(){color = color1}.color, // MediumSpringGreen
